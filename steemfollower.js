1226a50ce6967a81658924336f65cea3aeb51afe
kedimuren <35571501+kedimuren@users.noreply.github.com>
2018-01-20 21:06:06 +0300

    Add files via upload

---
+++ steemfollower.js
@@ -0,0 +1,38 @@
+         /**
+         *
+         * @author       kedimuren
+         * @date         2018.01.20
+         * @description  Auto Follower for steemit
+         */
+
+        var do_list      = "Who";
+        var count        = 0;
+
+		myFollower();
+			
+			
+		function myFollower(){
+			while(true){
+				var my_hollow_el = document.getElementsByClassName("slim hollow secondary");//all label [mute,follow,unfollow]
+				for (element in my_hollow_el) {
+					var single_hollow_el = my_hollow_el[element];
+					if (single_hollow_el.innerHTML === "Follow") {
+						count++;
+						var who_is_follower      = single_hollow_el.closest('tr').querySelectorAll("td")[1].querySelectorAll("tr a strong")[0].innerHTML;
+						var up_button            = document.getElementsByClassName("button tiny hollow float-right")[0];
+						var disable_button_count = document.getElementsByClassName("button tiny hollow float-right disabled").length;
+						if (disable_button_count > 0) {
+							console.log("count: " + count);
+						    return;
+						}
+						else {
+							//single_hollow_el.click();
+							console.log("Your nick is " + who_is_follower);
+						}
+					}
+				}
+				up_button.click();
+	
+			}
+		}
+      
